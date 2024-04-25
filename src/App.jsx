b17238488be9cd3598989eeda85a7149291971eb
GBastienDev <gambet.bastien.dev@gmail.com>
2024-04-25 16:27:56 +0200

    parallax fin

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import './App.scss';
+
+const App = () => {
+
+    return (
+        <>
+            <section>
+
+                <section className="parallax-1">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Parallax War</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">L'origine</h2>
+                <p className="desc">Avant de devenir un célèbre chevalier Jedi, et bien avant de se révéler l'âme la plus noire de la galaxie, Anakin Skywalker est un jeune esclave sur la planète Tatooine. La Force est déjà puissante en lui et il est un remarquable pilote de Podracer. Le maître Jedi Qui-Gon Jinn le découvre et entrevoit alors son immense potentiel. Pendant ce temps, l'armée de droïdes de l'insatiable Fédération du Commerce a envahi Naboo dans le cadre d'un plan secret des Sith visant à accroître leur pouvoir.
+                </p>
+                 <section className="parallax-2">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 1</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">La Formation</h2>
+                <p className="desc">Depuis le blocus de la planète Naboo, la République, gouvernée par le Chancelier Palpatine, connaît une crise. Un groupe de dissidents, mené par le sombre Jedi comte Dooku, manifeste son mécontentement. Le Sénat et la population intergalactique se montrent pour leur part inquiets. Certains sénateurs demandent à ce que la République soit dotée d'une armée pour empêcher que la situation ne se détériore. Padmé Amidala, devenue sénatrice, est menacée par les séparatistes et échappe à un attentat.
+                </p>
+                <section className="parallax-3">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 2</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">La chute</h2>
+                <p className="desc">La Guerre des Clones fait rage. Une franche hostilité oppose désormais le Chancelier Palpatine au Conseil Jedi. Anakin Skywalker, jeune Chevalier Jedi pris entre deux feux, hésite sur la conduite à tenir. Séduit par la promesse d'un pouvoir sans précédent, tenté par le côté obscur de la Force, il prête allégeance au maléfique Darth Sidious et devient Dark Vador.Les Seigneurs Sith s'unissent alors pour préparer leur revanche, qui commence par l'extermination des Jedi.
+                </p>
+                <section className="parallax-4">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 3</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">Rébellion</h2>
+                <p className="desc">Des individus ordinaires, pour rester fidèles à leurs valeurs, vont tester l'impossible au péril de leur vie. Ils n'avaient pas prévu de devenir héros, mais dans une époque de plus en plus sombre, ils vont devoir dérober les plans de l'Étoile de la mort, l'arme de destruction ultime de l'Empire.
+                </p>
+                <section className="parallax-5">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Rogue</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">L'espoir</h2>
+                <p className="desc">La guerre civile fait rage entre l'Empire galactique et l'Alliance rebelle. Capturée par les troupes de choc de l'Empereur menées par le sombre et impitoyable Dark Vador, la princesse Leia Organa dissimule les plans de l'Etoile Noire.
+                </p>
+                <section className="parallax-6">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 4</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">L'empire</h2>
+                <p className="desc">Malgré la destruction de l'Étoile noire, l'Empire maintient son emprise sur la galaxie, et poursuit sans relâche sa lutte contre l'Alliance rebelle. Basés sur la planète glacée de Hoth, les rebelles essuient un assaut des troupes impériales. Parvenus à s'échapper, la princesse Leia, Han Solo, Chewbacca et C-3P0 se dirigent vers Bespin, la cité des nuages gouvernée par Lando Calrissian, ancien compagnon de Han.
+                </p>
+                <section className="parallax-7">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 5</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">Le retour</h2>
+                <p className="desc">L'empire galactique est plus puissant que jamais : la construction de la nouvelle arme, l'étoile de la mort, menace l'univers tout entier... Han Solo est remis à l'ignoble contrebandier Jabba le Hutt par le chasseur de primes Boba Fett. après l'échec d'une première tentative d'évasion menée par la princesse Leia, également arrêtée par Jabba, Luke Skywalker et Lando parviennent à libérer leurs amis.
+                </p>
+                <section className="parallax-8">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 6</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">Le réveil</h2>
+                <p className="desc">Plus de 30 ans après la bataille d'Endor, la galaxie n'en a pas fini avec la tyrannie et l'oppression. Les membres de l'Alliance rebelle, devenus la "Résistance," combattent les vestiges de l'Empire réunis sous la bannière du "Premier Ordre." Un mystérieux guerrier, Kylo Ren, semble vouer un culte à Dark Vador. Au même moment, une jeune femme nommée Rey, pilleuse d'épaves sur la planète désertique Jakku, va faire la rencontre de Finn, un Stormtrooper en fuite.
+                </p>
+                <section className="parallax-9">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 7</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">Le dernier</h2>
+                <p className="desc">Le Premier Ordre étend ses tentacules aux confins de l'univers, poussant la Résistance dans ses retranchements. Impossible de se sauver à la vitesse de la lumière avec cet ennemi continuellement aux trousses. Cela n'empêche pas Finn et ses camarades de tenter d'identifier une brèche chez leur adversaire. Pendant ce temps, Rey se trouve toujours sur la planète Ahch-To pour convaincre Luke Skywalker de lui enseigner les rudiments de la Force.
+                </p>
+                <section className="parallax-10">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 8</h1>
+                    </div>
+                </section>
+
+                <h2 className="title">Héritage</h2>
+                <p className="desc">Un an a passé depuis que Kylo Ren a tué Snoke, le Leader suprême et pris sa place. Bien que largement décimée, la Résistance est prête à renaître de ses cendres. Rey, Poe, Leia et leurs alliés se préparent à reprendre le combat. Mais ils vont devoir faire face à un vieil ennemi : l'empereur Palpatine.
+                </p>
+                <section className="parallax-11">
+                    <div className="parallax-inner">
+                        <h1 className="parallax-inner__title">Episode 9</h1>
+                    </div>
+                </section>
+
+                <p className="footer">Tous droit reservé.</p>
+               
+            </section>
+
+        </>
+    )
+}
+
+export default App;
